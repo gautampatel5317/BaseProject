58fe6af3af41b74edeeba7bb4ffde1e7e752f12e
Gautam Patel <gautampatel5317@gmail.com>
2020-07-22 16:22:57 +0530

    css/js

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,8 +1,14 @@
 @extends('frontend.layouts.app')
+@section('sidebar')
+@endsection
+@section('sidebar-footer')
+@endsection
 @section('header')
 @endsection
 @section('conent')
 <div class="container">
 @include('flash::message')
 </div>
+@endsection
+@section('after-script')
 @endsection
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,99 +1,78 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <title>{{ app_name() }}</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-  {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> --}}
-  <link rel="stylesheet" type="text/css" href="{{ \URL::to('/css/backend/bootstrap.min.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ \URL::to('/css/backend/custom.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ \URL::to('/css/backend/select2.min.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ \URL::to('/css/backend/adminltev3.css') }}">
-  <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */
-    .navbar {
-      margin-bottom: 0;
-      border-radius: 0;
-    }
-
-    /* Add a gray background color and some padding to the footer */
-    footer {
-      background-color: #f2f2f2;
-      padding: 25px;
-    }
-  </style>
-</head>
-<body>
-@yield('header')
-<nav class="navbar navbar-expand-lg navbar-light bg-primary">
-  <a class="navbar-brand" href="#"><img style="width: 135px;height: 56px;" src="{{ \URL::to('images/site_logos.png') }}" alt="Ticket5Star" class=""></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-    @if (!\Auth::user())
-      <ul class="navbar-nav my-2 my-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('customer.create') }}">Register <span class="sr-only">(current)</span></a>
-      </li>
-       <li class="nav-item active">
-        <a class="nav-link" href="{{ route('login') }}">Login <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-   @else
-   <ul class="navbar-nav my-2 my-lg-0">
-    <li class="nav-item active">
-       <span class="nav-link">{{ auth()->user()->name }}</span>
-     </li>
-      <li class="nav-item active">
-       <a href="#" class="nav-link" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
-                        <p>
-                            <i class="fas fa-sign-out-alt">
-                            </i>
-                            <span>{{ trans('global.logout') }}</span>
-                        </p>
-                    </a>
-      </li>
-
-   </ul>
-  @endif
-    <form class="form-inline my-2 my-lg-0">
-
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-<div class="jumbotron">
-  <div class="container text-center">
-    <h1>{{ app_name() }}</h1>
-    <p>Ticketmaster</p>
-  </div>
-</div>
-@yield('conent')
-<div class="container-fluid bg-3 text-center">
-</div>
-<footer class="container-fluid text-center">
-</footer>
-<form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
+  <head>
+    <title>{{ app_name() }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ \URL::to('/css/frontend/css/style.css') }}">
+  </head>
+  <body>
+    <div class="wrapper d-flex align-items-stretch">
+      <nav id="sidebar" class="">
+        <h1><a href="index.html" class="logo">{{ trans('Multiple Vendor') }}</a></h1>
+        @yield('sidebar')
+        <ul class="list-unstyled components mb-5">
+          <li class="active">
+            <a href="#"><span class="fa fa-home"></span> Home</a>
+          </li>
+          <li>
+            <a href="#"><span class="fa fa-user"></span> Product</a>
+          </li>
+          <li class="nav-item">
+                <a href="#" onclick="event.preventDefault(); document.getElementById('logoutform').submit();"><span class="fa  fa-sign-out"></span> Logout</a>
+          </li>
+        </ul>
+        @yield('sidebar-footer')
+        <div class="footer">
+          <p>
+            Copyright &copy;
+            <script>document.write(new Date().getFullYear());
+            </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+          </p>
+        </div>
+      </nav>
+      <!-- Page Content  -->
+      <div id="content" class="p-4 p-md-5">
+        @yield('header')
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <button type="button" id="sidebarCollapse" class="btn btn-primary">
+            <i class="fa fa-bars"></i>
+            <span class="sr-only">Toggle Menu</span>
+            </button>
+            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Product</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        @yield('content')
+        <h2 class="mb-4">Sidebar #07</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+    <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
                 {{ csrf_field() }}
             </form>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-{{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> --}}
-<script type="text/javascript" src="{{ \URL::to('js/backend/bootstrap.min.js') }}"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
-<script type="text/javascript" src="{{ \URL::to('js/backend/select2.full.min.js') }}"></script>
-<script type="text/javascript" src="{{ \URL::to('js/backend/adminlte.js') }}"></script>
-@yield('script')
-</body>
+    @yield('after-script')
+    <script src="{{ \URL::to('js/frontend/js/jquery.min.js') }}"></script>
+    <script src="{{ \URL::to('js/frontend/js/popper.js') }}"></script>
+    <script src="{{ \URL::to('js/frontend/js/bootstrap.min.js')}}"></script>
+    <script src="{{ \URL::to('js/frontend/js/main.js')}}"></script>
+  </body>
 </html>
