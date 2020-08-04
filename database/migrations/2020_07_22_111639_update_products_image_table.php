9176afba6ff4fa25ab9e5adbfb9ee6b9ae724bac
gautampatel5317 <dashrath7021@gmail.com>
2020-08-04 17:16:56 +0530

    Product & sellet module pending change: merge branch

---
+++ database/migrations/2020_07_22_111639_update_products_image_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateProductsImageTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('products_files', function (Blueprint $table) {
+				$table->bigIncrements('id');
+				$table->unsignedInteger('product_id');
+				// $table->foreign('product_id')->references('id')->on('products');
+				$table->string('image');
+				$table->timestamps();
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('products_files');
+	}
+}
